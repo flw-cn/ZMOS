e6c7205a61eca0f9d247b4c1f747f90395c8c6cc
flw <flw@cpan.org>
2020-07-06 22:19:03 +0800

    Feat: 为普通用户增加 sudo 权限

--- XDE/flw-install-stage1-run-as-root.sh
+++ XDE/flw-install-stage1-run-as-root.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env sh
+
+if [ $USER = "root" -o $(id -un) != "root" ]; then
+    echo Please login as a non-root user, then su switch to root and execute this script.
+    return
+fi
+
 pkg install sudo vim zsh tmux ripgrep python3
+
+echo "$USER ALL=(ALL) ALL" > /usr/local/etc/sudoers.d/$USER
